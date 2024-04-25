1ede910deaf2c9b0faacad6aff0213ca212f027b
Bishal Giri <giribsaal@gmail.com>
2024-04-25 11:45:38 +0900

    re-export check export * re-export only the named export from the file

---
+++ node-concept/package/module.ts
@@ -0,0 +1,6 @@
+import { b } from './index';
+import a from './index';
+
+console.log(a);
+
+console.log(b());
---
+++ node-concept/package/re-export.ts
@@ -0,0 +1,8 @@
+const a = 10;
+
+const b = function() {
+    return 'Hello world!';
+}
+
+export { a, b };
+export default a;
